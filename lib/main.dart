a58c0084bd82d2df479ac97e147bbc26c4650f7d
Rafael <rafadangeloferraz@gmail.com>
2023-03-30 21:51:35 -0300

    commit para clonar projeto

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
+//TODO: TESTE CLONAGEM DE PROJETO
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,13 +44,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
