dd9c7aa80c2c98a6a13af22d885d480635eea17d
Rafael <rafadangeloferraz@gmail.com>
2023-05-04 07:58:14 -0300

    teste novo commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Teste Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Teste Clone Home Page'),
     );
   }
 }
